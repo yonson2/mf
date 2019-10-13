333c3a1bebc95317a353d994c4671d08ebd0ead1
Peter S <s@pramos.me>
2019-10-13 02:39:43 +0200

    feat(search): add option to search for more content than anime

--- mf.go
+++ mf.go
@@ -15,17 +15,18 @@ import (
 func main() {
 	resultsNo := flag.Int("n", 5, "Number of results to be displayed")
 	isSearch := flag.Bool("s", false, "Search for results instead of auto selecting the best match")
+	isGeneric := flag.Bool("g", false, "Search for all multimedia content (tv/movies) instead of just anime")
 	flag.Parse()
 	searchQuery := strings.Join(flag.Args(), " ")
 	if len(flag.Args()) == 0 {
-		fmt.Println("Missing name to search. try: \nleetflix <content to stream>")
+		fmt.Println("Missing name to search. try: \nmf <content to stream>")
 		os.Exit(1)
 	}
 	s := spinner.New(spinner.CharSets[7], 100*time.Millisecond)
 	s.Suffix = " Searching..."
 	s.Color("green", "bold")
 	s.Start()
-	results, err := search.Search(searchQuery, *resultsNo)
+	results, err := search.Search(searchQuery, *resultsNo, *isGeneric)
 	s.Stop()
 	if err != nil {
 		log.Println("Error searching for results", err)
